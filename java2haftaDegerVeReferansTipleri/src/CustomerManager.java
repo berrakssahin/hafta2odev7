9945c5fdd9b010d21e6460830ee735646532abb7
berrakssahin <112779654+berrakssahin@users.noreply.github.com>
2022-10-07 00:35:37 +0300

    Add files via upload

---
+++ java2haftaDegerVeReferansTipleri/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager{
+    public void Add(){
+        System.out.println("Müşteri silindi");
+    }
+
+    public void Remove() {
+        System.out.println("Müşteri silindi");
+    }
+
+    public void Update(){
+        System.out.println("Müşteri güncellendi");
+    }
+}
